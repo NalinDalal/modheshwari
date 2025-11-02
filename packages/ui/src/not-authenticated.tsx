48b3c5a135bd578298681953e296d04de7c9e547
Nalin <nalindalal2004@gmail.com>
2025-11-03 00:51:15 +0530

    types added

--- packages/ui/src/not-authenticated.tsx
+++ packages/ui/src/not-authenticated.tsx
@@ -18,14 +18,15 @@ export function notAuthenticated({
   children: React.ReactNode;
   className?: string;
 }): JSX.Element {
-  return;
-  <div className="flex flex-col items-center justify-center h-screen">
-    <div className="flex flex-col items-center justify-center">
-      {" "}
-      <h1 className="text-3xl font-bold mb-4">Not Authenticated</h1>
-      <p className="text-lg text-gray-600">
-        You are not authenticated to view this page.
-      </p>
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center">
+        {" "}
+        <h1 className="text-3xl font-bold mb-4">Not Authenticated</h1>
+        <p className="text-lg text-gray-600">
+          You are not authenticated to view this page.
+        </p>
+      </div>
     </div>
-  </div>;
+  );
 }
